6e9ec5be7132e58e7c8f0e6a35f45e5beaec4b36
daniel gusko <daniel.h.gusko@gmail.com>
2025-11-11 11:19:18 +0000

    - removed the test_exercises.py file - edited the run file by removing test_exercises run - completed and tested pipeline_functions.py file

--- course/intro/pipeline_functions.py
+++ course/intro/pipeline_functions.py
@@ -8,27 +8,32 @@ import plotly.express as px
 def plot_scatter(df, x_name, y_name):
     """Given a dataframe containing numeric columns specified by x_name and y_name
     return a plotly express scatterplot"""
-    return 0
+    chart = px.scatter(x=df[x_name], y=df[y_name])
+    return chart
 
 
 def calculate_correlation(df, x1, x2):
     """Given a dataframe containing numeric columns specified by x_name and y_name
     return two objects (numbers), the first is the pearson correlation coefficient, the second 
     the significance of this estimate"""
-    return 0
+    output = pearsonr(df[x1], df[x2])
+    return output
 
 
 def fit_regression(df, x_name, y_name):
     """Given a dataframe containing numeric columns specified by x_name and y_name
     return the stats models OLS fit of a regression model of y on x"""
-    return 0
+    model = sm.OLS(exog=df[x_name], endog=df[y_name])
+    results = model.fit()
+    return results
 
 
 def filter_data(df, year):
     """Given a dataframe of various rows including a column 'Year' and an integer year
     return a dataframe containing only those rows where the value in this column is less
     than the value of the supplied year"""
-    return 0
+    output = df[df['Year'] < year]
+    return output
 
 
 def tyler_viglen():
